aff6f9de9d4927e794ff655ec9d122dd0c14796c
italic <italic.rendezvous@gmail.com>
2018-08-03 22:12:31 -0700

    Cleanup: simplify wacom mappings. Update with current screen config: horizontal on left, vertical on right.

--- bash/wacom_cintiq13hd.sh
+++ bash/wacom_cintiq13hd.sh
@@ -1,100 +1,73 @@
 #!/bin/bash
 
-if [ -e /home/italic/.cintiq_dual ]
-then
+# Screen/Cintiq dimensions: 1920 x 1080 (1.7778)
+# Tablet dimensions: 58052 x 33348 (1.7408)
+# XY: 700 400 58752 33748
 
-  xsetwacom --set "Wacom Cintiq 13HD stylus" MapToOutput "HEAD-1"
-  xsetwacom --set "Wacom Cintiq 13HD stylus" Area 700 400 58752 33748
-  xsetwacom --set "Wacom Cintiq 13HD stylus" Button 3 2
-  xsetwacom --set "Wacom Cintiq 13HD stylus" Button 2 3
-  xsetwacom --set "Wacom Cintiq 13HD stylus" TabletPCButton on
-  xsetwacom --set "Wacom Cintiq 13HD stylus" Mode Absolute
-  xsetwacom --set "Wacom Cintiq 13HD stylus" Suppress 8
-  xsetwacom --set "Wacom Cintiq 13HD stylus" RawSample 6
+# Screen: 3840 x 1080 (3.5556) horizontal side-by-side
+# Tablet: 65024 x 18288 (3.5556)
+# XY: 800 8981 58752 24867
 
-  xsetwacom --set "Wacom Cintiq 13HD eraser" MapToOutput "HEAD-1"
-  xsetwacom --set "Wacom Cintiq 13HD eraser" Area 700 400 58752 33748
-  xsetwacom --set "Wacom Cintiq 13HD eraser" Button 3 2
-  xsetwacom --set "Wacom Cintiq 13HD eraser" Button 2 3
-  xsetwacom --set "Wacom Cintiq 13HD eraser" TabletPCButton on
-  xsetwacom --set "Wacom Cintiq 13HD eraser" Mode Absolute
-  xsetwacom --set "Wacom Cintiq 13HD eraser" Suppress 8
-  xsetwacom --set "Wacom Cintiq 13HD eraser" RawSample 6
+# Screen: 3000 x 1920 (1.5625) horizontal + vertical
+# Tablet: 52106 x 33348 (1.5625)
+# XY: 800 400 52906 33348
 
-  xsetwacom --set "Wacom Cintiq 13HD pad" Button 1 "key super c"
-  xsetwacom --set "Wacom Cintiq 13HD pad" Button 2 "key ctrl z"
-  xsetwacom --set "Wacom Cintiq 13HD pad" Button 3 "key ctrl shift z"
 
-  xsetwacom --set "Wacom Cintiq 13HD pad" Button 8 "key e"
-  xsetwacom --set "Wacom Cintiq 13HD pad" Button 9 "key x"
+AREA_SINGLE="700 400 58752 33748"
+AREA_HH="800 8981 58752 24867"
+AREA_HV="800 400 52906 33348"
 
-  rm /home/italic/.cintiq_dual
-  touch /home/italic/.cintiq_single
+_set() {
+	xsetwacom --set "Wacom Cintiq 13HD stylus" Button 3 2
+	xsetwacom --set "Wacom Cintiq 13HD stylus" Button 2 3
+	xsetwacom --set "Wacom Cintiq 13HD stylus" TabletPCButton on
+	xsetwacom --set "Wacom Cintiq 13HD stylus" Mode Absolute
+	xsetwacom --set "Wacom Cintiq 13HD stylus" Suppress 8
+	xsetwacom --set "Wacom Cintiq 13HD stylus" RawSample 6
 
-  echo "Mapping to Screen 2 and 13HD"
+	xsetwacom --set "Wacom Cintiq 13HD eraser" Button 3 2
+	xsetwacom --set "Wacom Cintiq 13HD eraser" Button 2 3
+	xsetwacom --set "Wacom Cintiq 13HD eraser" TabletPCButton on
+	xsetwacom --set "Wacom Cintiq 13HD eraser" Mode Absolute
+	xsetwacom --set "Wacom Cintiq 13HD eraser" Suppress 8
+	xsetwacom --set "Wacom Cintiq 13HD eraser" RawSample 6
 
-elif [ -e /home/italic/.cintiq_single ]
-then
+	xsetwacom --set "Wacom Cintiq 13HD pad" Button 1 "key super c"
+	xsetwacom --set "Wacom Cintiq 13HD pad" Button 2 "key ctrl z"
+	xsetwacom --set "Wacom Cintiq 13HD pad" Button 3 "key ctrl shift z"
 
-  xsetwacom --set "Wacom Cintiq 13HD stylus" MapToOutput "desktop"
-  xsetwacom --set "Wacom Cintiq 13HD stylus" Area 800 8981 58752 24867
-  xsetwacom --set "Wacom Cintiq 13HD stylus" Button 3 2
-  xsetwacom --set "Wacom Cintiq 13HD stylus" Button 2 3
-  xsetwacom --set "Wacom Cintiq 13HD stylus" TabletPCButton on
-  xsetwacom --set "Wacom Cintiq 13HD stylus" Mode Absolute
-  xsetwacom --set "Wacom Cintiq 13HD stylus" Suppress 8
-  xsetwacom --set "Wacom Cintiq 13HD stylus" RawSample 6
+	xsetwacom --set "Wacom Cintiq 13HD pad" Button 8 "key e"
+	xsetwacom --set "Wacom Cintiq 13HD pad" Button 9 "key x"
+}
 
-  xsetwacom --set "Wacom Cintiq 13HD eraser" MapToOutput "desktop"
-  xsetwacom --set "Wacom Cintiq 13HD eraser" Area 800 8981 58752 24867
-  xsetwacom --set "Wacom Cintiq 13HD eraser" Button 3 2
-  xsetwacom --set "Wacom Cintiq 13HD eraser" Button 2 3
-  xsetwacom --set "Wacom Cintiq 13HD eraser" TabletPCButton on
-  xsetwacom --set "Wacom Cintiq 13HD eraser" Mode Absolute
-  xsetwacom --set "Wacom Cintiq 13HD eraser" Suppress 8
-  xsetwacom --set "Wacom Cintiq 13HD eraser" RawSample 6
+set_to_desktop() {
+	xsetwacom --set "Wacom Cintiq 13HD stylus" MapToOutput "desktop"
+	xsetwacom --set "Wacom Cintiq 13HD stylus" Area ${AREA_HV}
 
-  xsetwacom --set "Wacom Cintiq 13HD pad" Button 1 "key super c"
-  xsetwacom --set "Wacom Cintiq 13HD pad" Button 2 "key ctrl z"
-  xsetwacom --set "Wacom Cintiq 13HD pad" Button 3 "key ctrl shift z"
+	xsetwacom --set "Wacom Cintiq 13HD eraser" MapToOutput "desktop"
+	xsetwacom --set "Wacom Cintiq 13HD eraser" Area ${AREA_HV}
 
-  xsetwacom --set "Wacom Cintiq 13HD pad" Button 8 "key e"
-  xsetwacom --set "Wacom Cintiq 13HD pad" Button 9 "key x"
+	_set
 
-  rm /home/italic/.cintiq_single
-  touch /home/italic/.cintiq_dual
+	rm $HOME/.cintiq_single
+	touch $HOME/.cintiq_dual
+}
 
-  echo "Mapping to desktop"
+set_to_single() {
+	xsetwacom --set "Wacom Cintiq 13HD stylus" MapToOutput "HEAD-1"
+	xsetwacom --set "Wacom Cintiq 13HD stylus" Area ${AREA_SINGLE}
 
+	xsetwacom --set "Wacom Cintiq 13HD eraser" MapToOutput "HEAD-1"
+	xsetwacom --set "Wacom Cintiq 13HD eraser" Area ${AREA_SINGLE}
+
+	_set
+
+	rm $HOME/.cintiq_dual
+	touch $HOME/.cintiq_single
+}
+
+if [ -e $HOME/.cintiq_dual ]; then
+	set_to_single
 else
-
-  xsetwacom --set "Wacom Cintiq 13HD stylus" MapToOutput "desktop"
-  xsetwacom --set "Wacom Cintiq 13HD stylus" Area 800 8981 58752 24867
-  xsetwacom --set "Wacom Cintiq 13HD stylus" Button 3 2
-  xsetwacom --set "Wacom Cintiq 13HD stylus" Button 2 3
-  xsetwacom --set "Wacom Cintiq 13HD stylus" TabletPCButton on
-  xsetwacom --set "Wacom Cintiq 13HD stylus" Mode Absolute
-  xsetwacom --set "Wacom Cintiq 13HD stylus" Suppress 8
-  xsetwacom --set "Wacom Cintiq 13HD stylus" RawSample 6
-
-  xsetwacom --set "Wacom Cintiq 13HD eraser" MapToOutput "desktop"
-  xsetwacom --set "Wacom Cintiq 13HD eraser" Area 800 8981 58752 24867
-  xsetwacom --set "Wacom Cintiq 13HD eraser" Button 3 2
-  xsetwacom --set "Wacom Cintiq 13HD eraser" Button 2 3
-  xsetwacom --set "Wacom Cintiq 13HD eraser" TabletPCButton on
-  xsetwacom --set "Wacom Cintiq 13HD eraser" Mode Absolute
-  xsetwacom --set "Wacom Cintiq 13HD eraser" Suppress 8
-  xsetwacom --set "Wacom Cintiq 13HD eraser" RawSample 6
-
-  xsetwacom --set "Wacom Cintiq 13HD pad" Button 1 "key super c"
-  xsetwacom --set "Wacom Cintiq 13HD pad" Button 2 "key ctrl z"
-  xsetwacom --set "Wacom Cintiq 13HD pad" Button 3 "key ctrl shift z"
-
-  xsetwacom --set "Wacom Cintiq 13HD pad" Button 8 "key e"
-  xsetwacom --set "Wacom Cintiq 13HD pad" Button 9 "key x"
-
-  touch /home/italic/.cintiq_dual
-
-  echo "Mapping to desktop"
-
+	set_to_desktop
 fi
--- bash/wacom_intuos4_lg.sh
+++ bash/wacom_intuos4_lg.sh
@@ -1,88 +1,69 @@
 #!/bin/bash
 
-if [ -e /home/italic/.intuos_dual ]
-then
+# Screen dimensions: 1920 x 1080 (16:9)
+# Tablet dimensions: 65024 x 40640 (16:10)
+# XY: 0 2032 65024 38608
 
-  xsetwacom --set "Wacom Intuos4 8x13 stylus" MapToOutput "HEAD-0"
-  xsetwacom --set "Wacom Intuos4 8x13 stylus" Area 0 2032 65024 38608
-  xsetwacom --set "Wacom Intuos4 8x13 stylus" Button 3 2
-  xsetwacom --set "Wacom Intuos4 8x13 stylus" Button 2 3
-  xsetwacom --set "Wacom Intuos4 8x13 stylus" TabletPCButton on
-  xsetwacom --set "Wacom Intuos4 8x13 stylus" Mode Absolute
+# Screen: 3840 x 1080 (3.5556) horizontal side-by-side
+# Tablet: 65024 x 18288 (3.5556)
+# XY: 0 11176 65024 29464
 
-  xsetwacom --set "Wacom Intuos4 8x13 eraser" MapToOutput "HEAD-0"
-  xsetwacom --set "Wacom Intuos4 8x13 eraser" Area 0 2032 65024 38608
-  xsetwacom --set "Wacom Intuos4 8x13 eraser" Button 3 2
-  xsetwacom --set "Wacom Intuos4 8x13 eraser" Button 2 3
-  xsetwacom --set "Wacom Intuos4 8x13 eraser" TabletPCButton on
-  xsetwacom --set "Wacom Intuos4 8x13 eraser" Mode Absolute
+# Screen: 3000 x 1920 (1.5625) horizontal + vertical
+# Tablet: 63500 x 40640 (1.5625)
+# XY: 762 0 64262 40640
 
-  xsetwacom --set "Wacom Intuos4 8x13 pad" Button 1 "key super i"
-  xsetwacom --set "Wacom Intuos4 8x13 pad" Button 2 "key ctrl z"
-  xsetwacom --set "Wacom Intuos4 8x13 pad" Button 3 "key ctrl shift z"
 
-  xsetwacom --set "Wacom Intuos4 8x13 pad" Button 8 "key e"
-  xsetwacom --set "Wacom Intuos4 8x13 pad" Button 9 "key x"
+AREA_SINGLE="0 2032 65024 38608"
+AREA_HH="0 11176 65024 38608"
+AREA_HV="762 0 64262 40640"
 
-  rm /home/italic/.intuos_dual
-  touch /home/italic/.intuos_single
+_set() {
+	xsetwacom --set "Wacom Intuos4 8x13 stylus" Button 3 2
+	xsetwacom --set "Wacom Intuos4 8x13 stylus" Button 2 3
+	xsetwacom --set "Wacom Intuos4 8x13 stylus" TabletPCButton on
+	xsetwacom --set "Wacom Intuos4 8x13 stylus" Mode Absolute
 
-  echo "Mapping to Screen 1"
+	xsetwacom --set "Wacom Intuos4 8x13 eraser" Button 3 2
+	xsetwacom --set "Wacom Intuos4 8x13 eraser" Button 2 3
+	xsetwacom --set "Wacom Intuos4 8x13 eraser" TabletPCButton on
+	xsetwacom --set "Wacom Intuos4 8x13 eraser" Mode Absolute
 
-elif [ -e /home/italic/.intuos_single ]
-then
+	xsetwacom --set "Wacom Intuos4 8x13 pad" Button 1 "key super i"
+	xsetwacom --set "Wacom Intuos4 8x13 pad" Button 2 "key ctrl z"
+	xsetwacom --set "Wacom Intuos4 8x13 pad" Button 3 "key ctrl shift z"
 
-  xsetwacom --set "Wacom Intuos4 8x13 stylus" MapToOutput "desktop"
-  xsetwacom --set "Wacom Intuos4 8x13 stylus" Area 0 11176 65024 29464
-  xsetwacom --set "Wacom Intuos4 8x13 stylus" Button 3 2
-  xsetwacom --set "Wacom Intuos4 8x13 stylus" Button 2 3
-  xsetwacom --set "Wacom Intuos4 8x13 stylus" TabletPCButton on
-  xsetwacom --set "Wacom Intuos4 8x13 stylus" Mode Absolute
+	xsetwacom --set "Wacom Intuos4 8x13 pad" Button 8 "key e"
+	xsetwacom --set "Wacom Intuos4 8x13 pad" Button 9 "key x"
+}
 
-  xsetwacom --set "Wacom Intuos4 8x13 eraser" MapToOutput "desktop"
-  xsetwacom --set "Wacom Intuos4 8x13 eraser" Area 0 11176 65024 29464
-  xsetwacom --set "Wacom Intuos4 8x13 eraser" Button 3 2
-  xsetwacom --set "Wacom Intuos4 8x13 eraser" Button 2 3
-  xsetwacom --set "Wacom Intuos4 8x13 eraser" TabletPCButton on
-  xsetwacom --set "Wacom Intuos4 8x13 eraser" Mode Absolute
+set_to_single() {
+	xsetwacom --set "Wacom Intuos4 8x13 stylus" MapToOutput "HEAD-0"
+	xsetwacom --set "Wacom Intuos4 8x13 stylus" Area ${AREA_SINGLE}
 
-  xsetwacom --set "Wacom Intuos4 8x13 pad" Button 1 "key super i"
-  xsetwacom --set "Wacom Intuos4 8x13 pad" Button 2 "key ctrl z"
-  xsetwacom --set "Wacom Intuos4 8x13 pad" Button 3 "key ctrl shift z"
+	xsetwacom --set "Wacom Intuos4 8x13 eraser" MapToOutput "HEAD-0"
+	xsetwacom --set "Wacom Intuos4 8x13 eraser" Area ${AREA_SINGLE}
 
-  xsetwacom --set "Wacom Intuos4 8x13 pad" Button 8 "key e"
-  xsetwacom --set "Wacom Intuos4 8x13 pad" Button 9 "key x"
+	_set
 
-  rm /home/italic/.intuos_single
-  touch /home/italic/.intuos_dual
+	rm /home/italic/.intuos_dual
+	touch /home/italic/.intuos_single
+}
 
-  echo "Mapping to desktop"
+set_to_desktop() {
+	xsetwacom --set "Wacom Intuos4 8x13 stylus" MapToOutput "desktop"
+	xsetwacom --set "Wacom Intuos4 8x13 stylus" Area ${AREA_HV}
 
+	xsetwacom --set "Wacom Intuos4 8x13 eraser" MapToOutput "desktop"
+	xsetwacom --set "Wacom Intuos4 8x13 eraser" Area ${AREA_HV}
+
+	_set
+
+	rm /home/italic/.intuos_single
+	touch /home/italic/.intuos_dual
+}
+
+if [ -e /home/italic/.intuos_dual ]; then
+	set_to_single
 else
-
-  xsetwacom --set "Wacom Intuos4 8x13 stylus" MapToOutput "desktop"
-  xsetwacom --set "Wacom Intuos4 8x13 stylus" Area 0 11176 65024 29464
-  xsetwacom --set "Wacom Intuos4 8x13 stylus" Button 3 2
-  xsetwacom --set "Wacom Intuos4 8x13 stylus" Button 2 3
-  xsetwacom --set "Wacom Intuos4 8x13 stylus" TabletPCButton on
-  xsetwacom --set "Wacom Intuos4 8x13 stylus" Mode Absolute
-
-  xsetwacom --set "Wacom Intuos4 8x13 eraser" MapToOutput "desktop"
-  xsetwacom --set "Wacom Intuos4 8x13 eraser" Area 0 11176 65024 29464
-  xsetwacom --set "Wacom Intuos4 8x13 eraser" Button 3 2
-  xsetwacom --set "Wacom Intuos4 8x13 eraser" Button 2 3
-  xsetwacom --set "Wacom Intuos4 8x13 eraser" TabletPCButton on
-  xsetwacom --set "Wacom Intuos4 8x13 eraser" Mode Absolute
-
-  xsetwacom --set "Wacom Intuos4 8x13 pad" Button 1 "key super i"
-  xsetwacom --set "Wacom Intuos4 8x13 pad" Button 2 "key ctrl z"
-  xsetwacom --set "Wacom Intuos4 8x13 pad" Button 3 "key ctrl shift z"
-
-  xsetwacom --set "Wacom Intuos4 8x13 pad" Button 8 "key e"
-  xsetwacom --set "Wacom Intuos4 8x13 pad" Button 9 "key x"
-
-  touch /home/italic/.intuos_dual
-
-  echo "Mapping to desktop"
-
+	set_to_desktop
 fi
